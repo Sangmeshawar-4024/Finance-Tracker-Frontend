39b4729944f7dfd633afd69f14a2d1f603281f52
Sangmeshawar-4024 <nilasangameshwar425@gmail.com>
2025-07-30 09:38:52 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://finance-tracker-backend-4.onrender.com',
+  baseURL: 'https://finance-tracker-backend-a3xu.onrender.com/',
   headers: {
     'Content-Type': 'application/json',
   },
